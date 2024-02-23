a6a2e3e6f958a616860c39c6db642f93a815b233
chris4567890 <starchristian4@hotmail.com>
2024-02-23 13:19:18 +0100

    Update ShipmentDao.java
    done what I could

--- week03/opg6/taketwo/src/main/java/org/example/ShipmentDao.java
+++ week03/opg6/taketwo/src/main/java/org/example/ShipmentDao.java
@@ -2,6 +2,7 @@ package org.example;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Query;
 
 public class ShipmentDao {
 
@@ -15,8 +16,33 @@ public class ShipmentDao {
        em.getTransaction().commit();
        em.close();
    }
+    public void update_destination(Location destination,Integer id){
+       EntityManager em = emf.createEntityManager();
+       em.getTransaction().begin();
+       Query query = em.createQuery("update Shipment s set s.destination = :destination where id = :id");
+       query.setParameter("destination",destination);
+       query.setParameter("id",id);
+       em.getTransaction().commit();
+       em.close();
+    }
+    public void update_source(Location source,Integer id){
+       EntityManager em = emf.createEntityManager();
+       em.getTransaction().begin();
+       Query query = em.createQuery("update Shipment s set s.source = :source where id = :id");
+       query.setParameter("source",source);
+       query.setParameter("id",id);
+       em.getTransaction().commit();
+       em.close();
+    }
 
-
-
+    public void set_package(Package packs,Integer id){
+       EntityManager em = emf.createEntityManager();
+       em.getTransaction().begin();
+       Query query = em.createQuery("update Shipment s set s.packages = :packs where id = :id");
+       query.setParameter("packages",packs);
+       query.setParameter("id",id);
+       em.getTransaction().commit();
+       em.close();
+    }
 
 }
