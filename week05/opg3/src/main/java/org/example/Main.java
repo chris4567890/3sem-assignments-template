900c5e323669ee98a09d6db66c97cb0d815a13a9
chris4567890 <starchristian4@hotmail.com>
2024-03-22 19:33:17 +0100

    done as much as I can
    done as much as I can.

--- week05/opg3/src/main/java/org/example/Main.java
+++ week05/opg3/src/main/java/org/example/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         //System.out.println("Hello world!");
         ApplicationConfig applicationConfig = ApplicationConfig.getInstance();
-        applicationConfig.initiateServer().setExceptionHandling().startServer(7007).setRoutes(Routes.setRoutes());
+        applicationConfig.initiateServer().setExceptionHandling().checkSecurityRoles().startServer(7007).setRoutes(Routes.getSecuredRoutes());
 
 
     }
